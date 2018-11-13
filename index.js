a66db6299a666a234acd269fedebe2b520a24178
Joshua Newman <joshuanewman1993@gmail.com>
2018-11-13 11:05:45 +0000

    Completed the first one

--- index.js
+++ index.js
@@ -2,82 +2,34 @@
 //or number-like-strings and returns their sum.
 
 function sum(nums) {
-//let sum=0
-// for (let i=0; i < nums.length; i++)
-// sum+= nums[i]
-// return sum
-//}
-
- let sum = (nums).reduce((acc, val) => {
-  return acc + parseInt(val); /*parseInt turns a string to a nummber*/
- }, 0);
- return sum
+  if (nums.length === 0) return 0;
+  const total = nums.reduce((total, amount) => total + amount);
+  return total;
 }
-
 
 //How would we solve the problem of having an array of users and creating
 //an array of usernames of people who have logged in recently?  How can
 //we use reduce to do this ?
 
 function checkLoggedInRecently(users) {
-//let loggedInArray = []
-// for (let i=0; i < users.length; i++) {
-//    if (users[i].loggedInRecently === true) {
-//       loggedInArray.push(users[i].username)
-//    }
-//}
-//return loggedInArray
-//}
-
-let checkLoggedInArray = (users).reduce((acc, val) => {
-if (val.loggedInRecently === true) {
-acc.push(val.username) 
+  if (users.length === 0) {
+    return [];
+  }
+  const haveLoggedIn = users.reduce((acc, obj) => {
+    acc[obj.loggedInRecently] = true;
+    return acc;
+  }, {});
+  https://medium.com/front-end-hacking/stop-array-foreach-and-start-using-filter-map-some-reduce-functions-298b4dabfa09
 }
-return acc
-}, []);
-return checkLoggedInArray;
-}
-
 
 //How can we use reduce to return a flattened array from an input of
 //nested arrays ?
 
- function flattenArray(data) {
-//let flattened = [].concat.apply([], data);
-//    return flattened
-//}
-
-let flattenedArray = (data).reduce((acc, val) => {
-return acc.concat(val)
-}, []);
-return flattenedArray
-}
-
+function flattenArray(data) {}
 
 //Write a reduce that produces a tally-object with a count of each item
 //in the array;
 
-function tallyObject(desserts) {
-//let dessertsTally = {};
-//for (let i=0; i < desserts.length; i++){
-//  let dessert = desserts[i]
-//  if (!dessertsTally[dessert]) dessertsTally[dessert] = 1; /*same as normal if statement but w/o return { }*/
-//   else dessertsTally[dessert]++;
-//  }
-//  return dessertsTally;
-//}
+function tallyObject(desserts) {}
 
-const dessertTally = desserts.reduce((acc, val) => {
-  if (!acc[val]) acc[val] = 1;
-  else acc[val]++;
-  return acc;
-},{});
-return dessertTally
-}
-
-
-module.exports = { sum,
-checkLoggedInRecently,
-flattenArray,
-tallyObject
- };
+module.exports = { sum, checkLoggedInRecently, flattenArray, tallyObject };
--- spec/index.js
+++ spec/index.js
@@ -1,86 +1,98 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 const {
   sum,
   checkLoggedInRecently,
   flattenArray,
   tallyObject
-} = require('../index.js');
+} = require("../index.js");
 
-
-describe('sum', () => {
-  it('returns 0 when input is an empty array', () => {
+describe("sum", () => {
+  it("returns 0 when input is an empty array", () => {
     expect(sum([])).to.equal(0);
-   });
-  it('returns 1 when input is [1]', () => {
-   expect(sum([1])).to.equal(1);
   });
-  it('returns 3 when input is [1,2]', () => {
-    expect(sum([1,2])).to.equal(3);
+  it("returns 1 when input is [1]", () => {
+    expect(sum([1])).to.equal(1);
   });
-  it('returns 6 when input is [1,2,3]', () => {
-    expect(sum([1,2,3])).to.equal(6);
+  it("returns 3 when input is [1,2]", () => {
+    expect(sum([1, 2])).to.equal(3);
+  });
+  it("returns 6 when input is [1,2,3]", () => {
+    expect(sum([1, 2, 3])).to.equal(6);
   });
   it('returns 6 when input is an array of string-like-numbers ["1","2","3"]', () => {
-    expect(sum(['1','2','3'])).to.equal(6);
+    expect(sum(["1", "2", "3"])).to.equal(6);
   });
 });
 
-
-describe ('checkLoggedInRecently', () => {
-  it('returns an empty array when there are no users', () => {
+describe("checkLoggedInRecently", () => {
+  it("returns an empty array when there are no users", () => {
     expect(checkLoggedInRecently([])).to.eql([]);
-   });
-  it('returns a single username of a person who has logged in recently', () => {
-    expect(checkLoggedInRecently([ {
-      username: 'Paul',
-      loggedInRecently: true
-    }, ])).to.eql(['Paul'])
   });
-  it('returns an array of usernames of more than one person who has logged in recently', () => {
-    expect(checkLoggedInRecently([
-      {
-        username: 'Paul',
-        loggedInRecently: true
-      }, 
-      {
-        username: 'Ant',
-        loggedInRecently: true
-      },
-      {
-        username: 'Liz',
-        loggedInRecently: false
-      }
-    ])).to.eql(['Paul', 'Ant'])
+  it("returns a single username of a person who has logged in recently", () => {
+    expect(
+      checkLoggedInRecently([
+        {
+          username: "Paul",
+          loggedInRecently: true
+        }
+      ])
+    ).to.eql(["Paul"]);
+  });
+  it("returns an array of usernames of more than one person who has logged in recently", () => {
+    expect(
+      checkLoggedInRecently([
+        {
+          username: "Paul",
+          loggedInRecently: true
+        },
+        {
+          username: "Ant",
+          loggedInRecently: true
+        },
+        {
+          username: "Liz",
+          loggedInRecently: false
+        }
+      ])
+    ).to.eql(["Paul", "Ant"]);
   });
 });
 
-
-describe ('flattenArray', () => {
-  it('returns an empty array when given an empty array', () => {
+describe("flattenArray", () => {
+  it("returns an empty array when given an empty array", () => {
     expect(flattenArray([])).to.eql([]);
-   });
-  it('returns a single array if given a single array', () => {
-    expect(flattenArray([1,2,3])).to.eql([1,2,3]);
   });
-  it('returns a flattened array if given a two arrays', () => {
-    expect(flattenArray([[1,2,3], [4,5,6]])).to.eql([1,2,3,4,5,6]);
+  it("returns a single array if given a single array", () => {
+    expect(flattenArray([1, 2, 3])).to.eql([1, 2, 3]);
   });
-  it('returns a flattened array if given three or more arrays', () => {
-    expect(flattenArray([[1,2,3], [4,5,6],[7,8,9]])).to.eql([1,2,3,4,5,6,7,8,9]);
+  it("returns a flattened array if given a two arrays", () => {
+    expect(flattenArray([[1, 2, 3], [4, 5, 6]])).to.eql([1, 2, 3, 4, 5, 6]);
+  });
+  it("returns a flattened array if given three or more arrays", () => {
+    expect(flattenArray([[1, 2, 3], [4, 5, 6], [7, 8, 9]])).to.eql([
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9
+    ]);
   });
 });
 
-
-describe ('tallyObject', () => {
-  it('returns an empty object when there are no desserts in the array', () => {
+describe("tallyObject", () => {
+  it("returns an empty object when there are no desserts in the array", () => {
     expect(tallyObject([])).to.eql({});
-   });
-  it('returns a tally of a single item when every item in the array is the same', () => {
-    expect(tallyObject(['cake','cake','cake'])).to.eql({cake: 3});
-   });
-  it('returns a tally of a multiple items when the items in the array are mixed', () => {
-    expect(tallyObject(['cake', 'icecream', 'cookies', 'cake', 'icecream', 'cake'])).to.eql({cake : 3,
-      icecream : 2,
-      cookies : 1});
-   });
+  });
+  it("returns a tally of a single item when every item in the array is the same", () => {
+    expect(tallyObject(["cake", "cake", "cake"])).to.eql({ cake: 3 });
+  });
+  it("returns a tally of a multiple items when the items in the array are mixed", () => {
+    expect(
+      tallyObject(["cake", "icecream", "cookies", "cake", "icecream", "cake"])
+    ).to.eql({ cake: 3, icecream: 2, cookies: 1 });
+  });
 });
